064c68a9bb2ccf8be4310c5e75112545dca38570
Michael Hilt <michael_hilt@comcast.com>
2022-07-27 11:35:10 -0400

    declared html='' and then set that = to containerNode.innerHTML

--- script.js
+++ script.js
@@ -2,27 +2,32 @@ window.addEventListener("load", function() {
   fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
    let json = [];  
   // console.log(response);
-  response.json().then(function(json) {
-      const container = document.getElementById("container");
-     // let index = 0;
-      for (let i = 0; i < json.length; i++){
-      container.innerHTML += `<div class="astronaut">
-      <div class="bio">
-         <h3>${json[i].firstName} ${json[i].lastName}</h3>
-         <ul>
-            <li>Hours in space: ${json[i].hoursInSpace}</li>
-            <li>Active: ${json[i].active}</li>
-            <li>Skills: ${json[i].skills}</li>
-         </ul>
-      </div>
-      <img class="avatar" src="${json[i].picture}">
-   </div>
-   `
+  response.json().then(function(data) {
+
+     // data.sort(function(a, b)) { // to possibly sort the astronauts by hours in space
+     // return a.hoursInSpace - b.hoursInSpace;
+   //  };
+
+      let containerNode = document.getElementById("container");
+
+      let html = "";
+
+      for (let i = 0; i < data.length; i++){
+         let astronaut = data[i];
+         html += 
+         `<div class="astronaut">
+            <div class="bio">
+               <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                  <ul>
+                     <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                     <li>Active: ${astronaut.active}</li>
+                     <li>Skills: ${astronaut.skills}</li>
+                  </ul>
+            </div>
+            <img class="avatar" src="${astronaut.picture}">
+         </div>`
       };
+      containerNode.innerHTML = html;
     })
-
   })
-
-
-
 })
